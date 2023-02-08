8a6d916657e70e04776efdc1f6d2728278f18db2
Ruiyan Liang <Jliang@lcs.on.ca>
2023-02-08 10:56:13 -0500

    Initial Commit

---
+++ BookingHelper/BookingHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookingHelperApp.swift
+//  BookingHelper
+//
+//  Created by dyhidrogen monoxide on 2023/2/8.
+//
+
+import SwiftUI
+
+@main
+struct BookingHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
